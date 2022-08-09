eb831e910a78c9483a8f1f4d85c935b21ae3fd80
Sergejs Tiselskis <slokas85@gmail.con>
2022-08-09 13:51:26 +0300

    Initial Commit

---
+++ HWLesson2Module3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HWLesson2Module3
+//
+//  Created by John Doe on 09/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
